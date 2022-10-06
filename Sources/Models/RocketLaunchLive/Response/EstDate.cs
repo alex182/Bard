a5e2bb388ca5af79e27eb73a2977d304033363ac
Alex.Baker@coxautoinc.com <Alex.Baker@coxautoinc.com>
2022-10-05 21:58:29 -0500

    adding models for RocketLaunch Live API response

---
+++ Sources/Models/RocketLaunchLive/Response/EstDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bard.Sources.Models.RocketLaunchLive.Response
+{
+    public class EstDate
+    {
+        public int? Month { get; set; }
+        public int? Day { get; set; }
+        public int? Year { get; set; }
+        public int? Quarter { get; set; }
+    }
+}
